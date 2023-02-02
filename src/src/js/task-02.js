cfbeb40c0d8d0eae7dc6879af9c5bcf875627711
Ambr0s0v <106374502+Ambr0s0v@users.noreply.github.com>
2023-02-02 21:59:02 +0200

    update 2 task

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const categIngredients = document.querySelector("#ingredients");
+
+let ingredientsList = [];
+
+ ingredients.forEach((ingredient) => {
+    let item = document.createElement("li");
+    item.classList.add("item")
+    item.textContent = ingredient;
+    ingredientsList.push(item);
+})
+
+categIngredients.append(...ingredientsList)
